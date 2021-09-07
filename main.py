c3c8b023008db5b93467e310545c370c9e475083
rarina <rinah@hairun-technology.com>
2021-09-07 14:29:58 +0300

    Add requirement

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ load_dotenv(find_dotenv())
 WORKSPACE_JOB_ID = os.environ.get("WORKSPACE_JOB_ID")
 DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
 
-def base_flowchart_runner(event, context):
+def main(event, context):
      print("Env with func kub",WORKSPACE_JOB_ID)
 
      return "Good job" 
